680ce48479092c626ebe2ca7e2be9a6dad50715b
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-05-13 10:51:43 -0300

    feat: Inclusao das anotações Jakarta Bean Validation,classe Pessoa, pacote DTO

--- src/main/java/br/fai/lds/medlink/domain/dto/Person.java
+++ src/main/java/br/fai/lds/medlink/domain/dto/Person.java
@@ -1,11 +1,13 @@
 package br.fai.lds.medlink.domain.dto;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+
 
 
 import java.time.LocalDate;
@@ -21,9 +23,21 @@ public class Person {
     @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
     private String name;
 
+    @NotNull(message = "O CPF não pode ser nulo")
+    @Pattern(regexp = "^(\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2})$", message = "O CPF deve estar no formato XXX.XXX.XXX-XX")
     private String cpf;
+
+    @NotNull(message = "O gênero não pode ser nulo")
     private Gender gender;
+
+    @NotNull(message = "A data de nascimento não pode ser nula")
+    @Past(message = "A data de nascimento deve ser no passado")
     private LocalDate dataNascimento;
+
+    @NotNull(message = "O número de telefone não pode ser nulo")
+    @Pattern(regexp = "^\\(\\d{2}\\) \\d{4,5}-\\d{4}$", message = "O número de telefone deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX")
     private String phoneNumber;
+
+    @NotNull(message = "O endereço não pode ser nulo")
     private Address address;
 }
