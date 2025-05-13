0f1d6daa380a71f231ab88bdd7159fe0eeea7b0a
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-05-13 05:45:48 -0300

    feat: Inicializacao do projeto

---
+++ src/test/java/br/fai/lds/medlink/MedlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fai.lds.medlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
