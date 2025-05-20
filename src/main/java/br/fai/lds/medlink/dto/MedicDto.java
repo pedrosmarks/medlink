0655f2925be7b3344d4b2e6c893694168d8399ae
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-05-20 13:49:47 -0300

    feat: Criacao do DTO

---
+++ src/main/java/br/fai/lds/medlink/dto/MedicDto.java
@@ -0,0 +1,24 @@
+package br.fai.lds.medlink.dto;
+
+import br.fai.lds.medlink.domain.Address;
+import br.fai.lds.medlink.domain.Gender;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class MedicDto {
+
+    private int id;
+    private String name;
+    private String cpf;
+    private Gender gender;
+    private LocalDate dataNascimento;
+    private String phoneNumber;
+    private Address address;
+
+    private String crm;
+    private String specialty;
+    private String email;
+    private boolean active = true;
+}
---
+++ src/main/java/br/fai/lds/medlink/dto/PatientDto.java
@@ -0,0 +1,25 @@
+package br.fai.lds.medlink.dto;
+
+import br.fai.lds.medlink.domain.Address;
+import br.fai.lds.medlink.domain.Gender;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class PatientDto {
+
+
+    private int id;
+    private String name;
+    private String cpf;
+    private Gender gender;
+    private LocalDate dataNascimento;
+    private String phoneNumber;
+    private Address address;
+
+    private String email;
+    private String plan;
+    private String susCard;
+    private boolean active = true;
+
+}
