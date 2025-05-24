a38adee40e2c66fa3fd29b06de05aae886368305
pedrosmarks <pedrosmarks@hotmail.com>
2025-05-24 16:59:35 -0300

    feat: adicionado o inicialComponent e LoginBaseComponent com styles e rotas.

---
+++ Frontend/src/app/pages/inicial/inicial.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDesktop } from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faHospitalUser } from '@fortawesome/free-solid-svg-icons';
+import { faFileInvoice } from '@fortawesome/free-solid-svg-icons';
+import { faMessage } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-inicial',
+  standalone:true,
+  imports: [
+
+    FontAwesomeModule,
+    
+
+  ],
+  templateUrl: './inicial.component.html',
+  styleUrl: './inicial.component.css'
+})
+export class InicialComponent {
+
+  faDesktop = faDesktop;
+  faCircleUser = faCircleUser;
+  faHospitalUser = faHospitalUser;
+  faFileInvoice = faFileInvoice;
+  faMessage = faMessage;
+  faBell = faBell;
+
+}
---
+++ Frontend/src/app/pages/login/login-base/login-base.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-base',
+  imports: [],
+  templateUrl: './login-base.component.html',
+  styleUrl: './login-base.component.css'
+})
+export class LoginBaseComponent {
+
+}
