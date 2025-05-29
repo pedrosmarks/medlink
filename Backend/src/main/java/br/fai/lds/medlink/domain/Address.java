f840a26a03f30866ba272d94d4dedf0dadb3a240
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-05-28 23:22:37 -0300

    refactor: Refatoracao da classe Address

--- Backend/src/main/java/br/fai/lds/medlink/domain/Address.java
+++ Backend/src/main/java/br/fai/lds/medlink/domain/Address.java
@@ -1,19 +1,36 @@
 package br.fai.lds.medlink.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 
-@Data
-@Builder
+@Getter
+@AllArgsConstructor
 public class Address {
 
-    private String street;
-    private String number;
-    private String complement;
-    private String neighborhood;
-    private String city;
-    private String state;
-    private String zipCode;
+    @NotBlank(message = "Nome da rua não pode estar em branco")
+    private final String street;
+
+    @Size(min = 8, message = "Numero deve ter pelo menos 1 caractere")
+    private final String number;
+
+    private final String complement;
+
+    @NotBlank(message = "Bairro não pode estar em branco")
+    private final String neighborhood;
+
+    @NotBlank(message = "Cidade não pode estar em branco")
+    private final String city;
+
+    @NotBlank(message = "Estado não pode estar em branco")
+    private final String state;
+
+    @Pattern(regexp = "^\\d{5}-\\d{3}$", message = "CEP deve estar no formato XXXXX-XXX")
+    private final String zipCode;
 
 }
