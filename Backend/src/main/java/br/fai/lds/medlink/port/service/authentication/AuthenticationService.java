8b9065fc82c80bd9915020e1e3a1f6f6f55031b9
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-15 14:46:25 -0300

    feat: adicionar métodos de solicitação e redefinição de senha

--- Backend/src/main/java/br/fai/lds/medlink/port/service/authentication/AuthenticationService.java
+++ Backend/src/main/java/br/fai/lds/medlink/port/service/authentication/AuthenticationService.java
@@ -2,9 +2,15 @@ package br.fai.lds.medlink.port.service.authentication;
 
 import br.fai.lds.medlink.domain.Patient;
 import br.fai.lds.medlink.domain.Medic;
+import br.fai.lds.medlink.domain.dataTransferObject.Login.PasswordResetRequestDTO;
+import br.fai.lds.medlink.domain.dataTransferObject.Login.PasswordResetDTO;
 
 public interface AuthenticationService {
 
-    Patient authenticatePatient (final String email, final String password);
-    Medic authenticateMedic (final String email, final String password);
+    Patient authenticatePatient(final String email, final String password);
+    Medic authenticateMedic(final String email, final String password);
+
+    void requestPasswordReset(PasswordResetRequestDTO dto);
+    boolean resetPassword(PasswordResetDTO dto);
 }
+
