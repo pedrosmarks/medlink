1241c29c5f2c4646f772f8fa513f656ebbc6a369
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-15 14:37:40 -0300

    feat: Criacao da classe PasswordResetDTO para definir a senha

---
+++ Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/Login/PasswordResetDTO.java
@@ -0,0 +1,28 @@
+package br.fai.lds.medlink.domain.dataTransferObject.Login;
+
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+    // DTO usado para redefinir a senha e/ou ativar a conta,após o usuário informar o código de verificação.
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public class PasswordResetDTO {
+
+        @NotBlank(message = "O identificador (e-mail ou CPF) é obrigatório.")
+        private String identifier;
+
+        @NotBlank(message = "O código de verificação é obrigatório.")
+        private String verificationCode;
+
+        @NotBlank(message = "A nova senha é obrigatória.")
+        @Size(min = 6, max = 20, message = "A senha deve ter entre 6 e 20 caracteres.")
+        private String newPassword;
+    }
+
