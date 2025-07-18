cabe89c90cc5789a2e98502b2ff2e3f37a67faed
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-18 00:09:22 -0300

    feat: Implementa metodo findByPatientId

--- Backend/src/main/java/br/fai/lds/medlink/port/dao/medicalRecord/MedicalRecordDao.java
+++ Backend/src/main/java/br/fai/lds/medlink/port/dao/medicalRecord/MedicalRecordDao.java
@@ -4,4 +4,7 @@ import br.fai.lds.medlink.domain.MedicalRecord;
 import br.fai.lds.medlink.port.dao.crud.CrudDao;
 
 public interface MedicalRecordDao extends CrudDao<MedicalRecord> {
+
+    MedicalRecord findByPatientId(int patientId);
+
 }
