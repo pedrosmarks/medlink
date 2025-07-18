e877be2711110170923c540022492c25e7bcd784
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-17 22:33:41 -0300

    feat: Implementa metodo findByMedicId

--- Backend/src/main/java/br/fai/lds/medlink/port/dao/patient/PatientDao.java
+++ Backend/src/main/java/br/fai/lds/medlink/port/dao/patient/PatientDao.java
@@ -4,10 +4,14 @@ import br.fai.lds.medlink.domain.Patient;
 import br.fai.lds.medlink.port.dao.crud.CrudDao;
 import br.fai.lds.medlink.port.dao.crud.SoftDeleteDao;
 
+import java.util.List;
+
 public interface PatientDao extends CrudDao <Patient>, SoftDeleteDao {
 
     Patient readById(int id);
     Patient findByEmail(String email);
+    List<Patient> findByMedicId(int medicId);
+
 
 
 }
