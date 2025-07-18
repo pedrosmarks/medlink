252f68dbcb20224ebf4f85b107d0c7426a7fa88a
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-17 22:34:42 -0300

    feat: Implementa findByMedicId

--- Backend/src/main/java/br/fai/lds/medlink/port/service/patient/PatientService.java
+++ Backend/src/main/java/br/fai/lds/medlink/port/service/patient/PatientService.java
@@ -1,9 +1,17 @@
 package br.fai.lds.medlink.port.service.patient;
 
 import br.fai.lds.medlink.domain.Patient;
+import br.fai.lds.medlink.domain.dataTransferObject.Patient.PatientResponseDto;
 import br.fai.lds.medlink.port.service.crud.CrudService;
+
+import java.util.List;
 
 public interface PatientService extends CrudService<Patient> {
     boolean deactivate(int id);
     boolean delete(int id);
+
+    List<PatientResponseDto> getPatientsByMedicId(int medicId);
+
+    List<Patient> findByMedicId(int medicId);
+
 }
