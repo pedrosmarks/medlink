a31411058fcd19848cb8ed01d59aa44a3701391d
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-21 17:51:22 -0300

    feat: criacao da classe VaccineCreateDto

---
+++ Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/MedicalRecord/clinical/VaccineCreateDto.java
@@ -0,0 +1,35 @@
+package br.fai.lds.medlink.domain.dataTransferObject.MedicalRecord.clinical;
+
+import br.fai.lds.medlink.domain.Vaccine;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class VaccineCreateDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotNull(message = "Date is required")
+    private String date;
+
+    public Vaccine toEntity() {
+        return Vaccine.builder()
+                .name(this.name)
+                .date(this.date)
+                .build();
+    }
+
+    public static VaccineCreateDto fromEntity(Vaccine entity) {
+        if (entity == null) return null;
+        return VaccineCreateDto.builder()
+                .name(entity.getName())
+                .date(entity.getDate())
+                .build();
+    }
+}
+
