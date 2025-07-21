915957f364f80e1e110c5ac6306f95ff2fb4fe13
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-21 17:50:20 -0300

    feat: criacao da classe AllergyCreateDto

---
+++ Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/MedicalRecord/clinical/AllergyCreateDto.java
@@ -0,0 +1,37 @@
+package br.fai.lds.medlink.domain.dataTransferObject.MedicalRecord.clinical;
+
+import br.fai.lds.medlink.domain.Allergy;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@Builder
+public class AllergyCreateDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Substance is required")
+    private String substance;
+
+
+    @NotBlank(message = "Name is required")
+    private String reaction;
+
+    @NotBlank(message = "Name is required")
+    private String severity;
+
+    public Allergy toEntity() {
+        return Allergy.builder()
+                .substance(this.substance)
+                .build();
+    }
+
+    public static AllergyCreateDto fromEntity(Allergy entity) {
+        if (entity == null) return null;
+        return AllergyCreateDto.builder()
+                .substance(entity.getSubstance())
+                .build();
+    }
+}
+
