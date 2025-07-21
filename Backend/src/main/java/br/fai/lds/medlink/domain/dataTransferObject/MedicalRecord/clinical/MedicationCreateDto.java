f5af6e6b03cf97ad44ade536db16d48fcc12ebd4
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-21 17:51:53 -0300

    feat: criacao da classe MedicationCreateDto

---
+++ Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/MedicalRecord/clinical/MedicationCreateDto.java
@@ -0,0 +1,36 @@
+package br.fai.lds.medlink.domain.dataTransferObject.MedicalRecord.clinical;
+
+import br.fai.lds.medlink.domain.Medication;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@Builder
+public class MedicationCreateDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Dosage is required")
+    private String dosage;
+
+    @NotBlank(message = "Frequency is required")
+    private String frequency;
+
+    public Medication toEntity() {
+        return Medication.builder()
+                .name(this.name)
+                .dosage(this.dosage)
+                .frequency(this.frequency)
+                .build();
+    }
+
+    public static MedicationCreateDto fromEntity(Medication entity) {
+        if (entity == null) return null;
+        return MedicationCreateDto.builder()
+                .name(entity.getName())
+                .dosage(entity.getDosage())
+                .frequency(entity.getFrequency())
+                .build();
+    }
+}
