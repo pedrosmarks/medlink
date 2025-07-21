dedeb5b9b6fdeaea09410a6076ce4c06bcd28514
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-21 17:51:38 -0300

    feat: criacao da classe SurgeryCreateDto

---
+++ Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/MedicalRecord/clinical/SurgeryCreateDto.java
@@ -0,0 +1,42 @@
+package br.fai.lds.medlink.domain.dataTransferObject.MedicalRecord.clinical;
+
+import br.fai.lds.medlink.domain.Surgery;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class SurgeryCreateDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotNull(message = "Date is required")
+    private String date;
+
+    private String location;
+
+    private String notes;
+
+    public Surgery toEntity() {
+        return Surgery.builder()
+                .name(this.name)
+                .date(this.date)
+                .location(this.location)
+                .notes(this.notes)
+                .build();
+    }
+
+    public static SurgeryCreateDto fromEntity(Surgery entity) {
+        if (entity == null) return null;
+        return SurgeryCreateDto.builder()
+                .name(entity.getName())
+                .date(entity.getDate())
+                .location(entity.getLocation())
+                .notes(entity.getNotes())
+                .build();
+    }
+}
