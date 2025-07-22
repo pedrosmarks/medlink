51b37cf8357998136e5b90eb5d02c45d539cacde
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-22 14:31:48 -0300

    docs: Implementa comentarios para melhor entendimento da aplicação

--- Backend/src/main/java/br/fai/lds/medlink/controller/PatientController.java
+++ Backend/src/main/java/br/fai/lds/medlink/controller/PatientController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// Controlador REST responsável por gerenciar os paientes da aplicação
 @RestController
 @RequestMapping("/api/patients")
 public class PatientController {
