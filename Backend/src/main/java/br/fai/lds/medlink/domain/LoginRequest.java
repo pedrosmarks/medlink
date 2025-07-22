18ff67c21cff5dc991dcf18d3e23991b3925ace0
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-22 10:45:15 -0300

    docs: adicionar comentário para melhor entendimento da classe

--- Backend/src/main/java/br/fai/lds/medlink/domain/LoginRequest.java
+++ Backend/src/main/java/br/fai/lds/medlink/domain/LoginRequest.java
@@ -3,6 +3,8 @@ package br.fai.lds.medlink.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+
+// Representa os dados de entrada para autenticação do usuário.
 @Getter
 @Setter
 public class LoginRequest {
