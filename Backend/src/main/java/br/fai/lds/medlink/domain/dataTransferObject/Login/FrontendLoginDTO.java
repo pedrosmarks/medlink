2eaf82288a45a30e3648dbed9079b807bf82ad6e
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-08-22 01:34:17 -0300

    refactor: remove classe FrontendLoginDTO não utilizada

--- Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/Login/FrontendLoginDTO.java
+++
@@ -1,9 +0,0 @@
-package br.fai.lds.medlink.domain.dataTransferObject.Login;
-
-import lombok.Data;
-
-@Data
-public class FrontendLoginDTO {
-    private String usuario;
-    private String senha;
-}
