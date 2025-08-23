32102c8937e02e5e728124cc6d0e08eeec49dd29
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-08-23 14:27:42 -0300

    feat: implementa MedicResponseDto para resposta da AP

--- Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/Medic/MedicResponseDto.java
+++ Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/Medic/MedicResponseDto.java
@@ -1,20 +1,40 @@
 package br.fai.lds.medlink.domain.dataTransferObject.Medic;
 
+import br.fai.lds.medlink.domain.Address;
+import br.fai.lds.medlink.domain.Gender;
 import br.fai.lds.medlink.domain.Medic;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class MedicResponseDto {
 
+    private int id;
     private String name;
+    private String cpf;
+    private Gender gender;
+    private LocalDate birthDate;
+    private String phoneNumber;
+    private Address address;
     private String crm;
     private String specialty;
+    private String email;
+    private boolean active;
 
     public static MedicResponseDto fromEntity(Medic entity) {
         MedicResponseDto dto = new MedicResponseDto();
+        dto.setId(entity.getId());
         dto.setName(entity.getName());
+        dto.setCpf(entity.getCpf());
+        dto.setGender(entity.getGender());
+        dto.setBirthDate(entity.getBirthDate());
+        dto.setPhoneNumber(entity.getPhoneNumber());
+        dto.setAddress(entity.getAddress());
         dto.setCrm(entity.getCrm());
         dto.setSpecialty(entity.getSpecialty());
+        dto.setEmail(entity.getEmail());
+        dto.setActive(entity.isActive());
         return dto;
     }
 }
