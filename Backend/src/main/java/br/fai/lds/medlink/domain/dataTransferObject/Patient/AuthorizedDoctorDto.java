eb62288ec225c296c4da264434bf2a4200a0703f
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-08-25 14:33:57 -0300

    feat: adiciona DTO para médicos autorizados pelo paciente

---
+++ Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/Patient/AuthorizedDoctorDto.java
@@ -0,0 +1,14 @@
+package br.fai.lds.medlink.domain.dataTransferObject.Patient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorizedDoctorDto {
+    private int id;
+    private String nome;
+    private String especialidade;
+}
