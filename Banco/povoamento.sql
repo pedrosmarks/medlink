6aba989d2637351df49ca6abf7efcb260776f050
Augusto <senescal.oliveira@gmail.com>
2025-08-26 08:58:21 -0300

    feat: adicao do povoamento da tabela pessoa

--- Banco/povoamento.sql
+++ Banco/povoamento.sql
@@ -11,3 +11,10 @@ INSERT INTO cidade (estado_id, nome) VALUES
 (2, 'Rio de Janeiro'),
 (3, 'Belo Horizonte'),
 (4, 'Salvador');
+
+INSERT INTO pessoa (nome, cpf, sexo, data_nascimento) VALUES
+('Ana Silva', '12345678901', 'F', '1990-05-10'),
+('João Pereira', '12345678902', 'M', '1985-03-20'),
+('Maria Oliveira', '12345678903', 'F', '2000-08-15'),
+('Carlos Souza', '12345678904', 'M', '1978-11-25'),
+('Beatriz Lima', '12345678905', 'F', '1995-01-05');
