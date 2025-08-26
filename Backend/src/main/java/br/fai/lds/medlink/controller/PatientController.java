11d6d5f5f5addee717cda35b8534d77a9254fd15
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-08-25 23:31:10 -0300

    ix: atualiza PatientController para usar Consultation

--- Backend/src/main/java/br/fai/lds/medlink/controller/PatientController.java
+++ Backend/src/main/java/br/fai/lds/medlink/controller/PatientController.java
@@ -86,8 +86,8 @@ public class PatientController {
     // Endpoints para buscar dados médicos específicos do paciente
     
     @GetMapping("/{id}/consultations")
-    public ResponseEntity<ApiResponse<List<Consulta>>> getConsultationsByPatient(@PathVariable int id) {
-        return getPatientMedicalData(id, Patient::getConsultas, "Consultas recuperadas com sucesso.");
+    public ResponseEntity<ApiResponse<List<Consultation>>> getConsultationsByPatient(@PathVariable int id) {
+        return getPatientMedicalData(id, Patient::getConsultations, "Consultas recuperadas com sucesso.");
     }
 
     @GetMapping("/{id}/vaccines")
