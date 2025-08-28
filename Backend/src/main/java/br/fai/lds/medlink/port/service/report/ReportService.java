9d5c454eb375b6b60198938532da1697ef0b4b2a
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-08-28 17:38:36 -0300

    feat: adiciona getAvailableReportTypes e ReportTypeDto ao ReportService

--- Backend/src/main/java/br/fai/lds/medlink/port/service/report/ReportService.java
+++ Backend/src/main/java/br/fai/lds/medlink/port/service/report/ReportService.java
@@ -1,15 +1,15 @@
 package br.fai.lds.medlink.port.service.report;
 
-
 import br.fai.lds.medlink.domain.dataTransferObject.Report.BirthdayReportDto;
 import br.fai.lds.medlink.domain.dataTransferObject.Report.ConsultationReportDto;
 import br.fai.lds.medlink.domain.dataTransferObject.Report.PatientReportDto;
 import br.fai.lds.medlink.domain.dataTransferObject.Report.PlanTypeReportDto;
+import br.fai.lds.medlink.domain.dataTransferObject.Report.ReportTypeDto;
 
 import java.util.List;
 
-// Interface que define os métodos para geração de relatórios do sistema,
 public interface ReportService {
+    List<ReportTypeDto> getAvailableReportTypes();
     List<BirthdayReportDto> getBirthdayReport();
     List<PlanTypeReportDto> getPlanTypeReport();
     List<ConsultationReportDto> getConsultationReport();
