5935283e4fbee35ada5f71cc67899ac1af4d3477
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-08-30 22:22:54 -0300

    refactor: remove classes desnecessárias

--- Backend/src/main/java/br/fai/lds/medlink/MedlinkApplication.java
+++ Backend/src/main/java/br/fai/lds/medlink/MedlinkApplication.java
@@ -14,6 +14,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class MedlinkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MedlinkApplication.class, args);
+		SpringApplication.run(MedlinkApplication.class,  args);
 	}
 }
