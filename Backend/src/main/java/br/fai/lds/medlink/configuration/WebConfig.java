ad7ea11483d727e5165aa479dac8467018f95bc6
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-08-30 22:12:16 -0300

    config: configura CORS para integração frontend-backend

--- Backend/src/main/java/br/fai/lds/medlink/configuration/WebConfig.java
+++ Backend/src/main/java/br/fai/lds/medlink/configuration/WebConfig.java
@@ -11,8 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
