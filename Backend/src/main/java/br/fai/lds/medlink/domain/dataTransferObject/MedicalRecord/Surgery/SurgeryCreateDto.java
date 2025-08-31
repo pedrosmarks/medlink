7a856289b3837d3c1243115ac9abd38a26f2f49a
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-08-30 22:15:00 -0300

    fix: converte campo date para LocalDate no SurgeryCreateDto

--- Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/MedicalRecord/Surgery/SurgeryCreateDto.java
+++ Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/MedicalRecord/Surgery/SurgeryCreateDto.java
@@ -15,7 +15,7 @@ public class SurgeryCreateDto {
     private String name;
 
     @NotNull(message = "Date is required")
-    private String date;
+    private LocalDate date;
 
     private String location;
 
--- Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/Patient/SurgeryCreateDto.java
+++ Backend/src/main/java/br/fai/lds/medlink/domain/dataTransferObject/Patient/SurgeryCreateDto.java
@@ -3,13 +3,14 @@ package br.fai.lds.medlink.domain.dataTransferObject.Patient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SurgeryCreateDto {
     private String name;
-    private String date;
+    private LocalDate date;
     private String location;
     private String notes;
 }
