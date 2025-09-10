cb7c20d1a03e82756b9750201d0694d7808f6a7f
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-09-10 15:49:51 -0300

    feat: adiciona método findByIds ao MedicService para busca em lote

--- Backend/src/main/java/br/fai/lds/medlink/port/service/medic/MedicService.java
+++ Backend/src/main/java/br/fai/lds/medlink/port/service/medic/MedicService.java
@@ -3,9 +3,16 @@ package br.fai.lds.medlink.port.service.medic;
 import br.fai.lds.medlink.domain.Medic;
 import br.fai.lds.medlink.port.service.crud.CrudService;
 
+import java.util.List;
+import java.util.Map;
+
 public interface MedicService extends CrudService<Medic> {
 
-
-
+    /**
+     * Busca múltiplos médicos por IDs de uma vez para evitar N+1 queries
+     * @param ids Lista de IDs dos médicos
+     * @return Map com ID como chave e Medic como valor
+     */
+    Map<Integer, Medic> findByIds(List<Integer> ids);
 
 }
