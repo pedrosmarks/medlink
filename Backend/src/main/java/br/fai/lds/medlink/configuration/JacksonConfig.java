d7e1d3babb9a45f1979015b692f9ca971c8ce5f0
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-09-16 23:09:48 -0300

    refactor: correçoes

--- Backend/src/main/java/br/fai/lds/medlink/configuration/JacksonConfig.java
+++ Backend/src/main/java/br/fai/lds/medlink/configuration/JacksonConfig.java
@@ -1,4 +1,4 @@
-package br.fai.lds.medlink.config;
+package br.fai.lds.medlink.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
